ed6d2cef600fad0ec6b3419acb50ee99540195ec
Deeno171 <deeno171st@gmail.com>
2025-06-24 23:13:53 +0700

    Initial Commit

---
+++ Volleyball App/Volleyball_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volleyball_AppApp.swift
+//  Volleyball App
+//
+//  Created by E2617595 on 24/6/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Volleyball_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewGame()
+        }
+    }
+}
---
+++ Volleyball AppTests/Volleyball_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Volleyball_AppTests.swift
+//  Volleyball AppTests
+//
+//  Created by E2617595 on 24/6/2568 BE.
+//
+
+import Testing
+@testable import Volleyball_App
+
+struct Volleyball_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
